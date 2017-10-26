dbf2345914640ad50c8a20d8b203fecf976a7cae
Code-Omega <Clue@wirelessprv-10-195-54-63.near.illinois.edu>
2017-10-25 19:37:50 -0500

    add max_df

--- app/views.py
+++ app/views.py
@@ -98,6 +98,7 @@ for post in dSA.entries[:articles_per_source]:
 vectorizer = TfidfVectorizer(input = 'content',
                              norm = 'l2',
                              min_df = 1,
+                             max_df = 0.8,
                              stop_words = 'english',
                              ngram_range = (1, 3),
                              smooth_idf = False,
