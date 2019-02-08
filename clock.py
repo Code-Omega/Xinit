756b2de69dcb471dc130766554468e1fd418f2ab
Code-Omega <Clue@Zehuas-Air.home>
2019-02-08 00:44:38 -0500

    schedule doc vectorizer retraining; reset feed schedule

--- clock.py
+++ clock.py
@@ -19,19 +19,26 @@ MONGO_URI = 'mongodb://xibot:jacqep-4fubJy-ruhcoq@ds255784.mlab.com:55784/xinit'
 ##########################################################################
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=30)
 def process_feeds():
-    print('Getting feed data, runs every 30 minutes.')
+    print('Getting feed data; runs every 30 minutes.')
     app = Flask(__name__)
     app.config['MONGO_URI'] = MONGO_URI
     mongo = PyMongo(app)
 
     num_new_posts = ana.get_feeds(mongo)
     print('# new posts:',num_new_posts)
-    if num_new_posts > -1:
+    if num_new_posts > 0:
         print('Process with new feeds.')
         ana.process_feeds(mongo, num_posts = 10, topNum = 3)
         print('Feeds processed')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=20)
+def retrain_doc_model():
+    print('Retrain the tfidf model; runs every day at night.')
+    ana.update_doc_model(mongo)
+    print('Model retrained')
 
 
 # @sched.scheduled_job('interval', minutes=30)
