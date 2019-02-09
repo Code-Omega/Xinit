1999de4e1050af621d9f90c80374cec57ae9d246
Code-Omega <Clue@Zehuas-Air.home>
2019-02-09 00:53:07 -0500

    landing page in the making

--- app/views.py
+++ app/views.py
@@ -378,6 +378,11 @@ def logout():
     return redirect(url_for('index'))
 
 
+@app.route('/landing')
+def landing():
+    return render_template('landing.html', title='Xinit')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
